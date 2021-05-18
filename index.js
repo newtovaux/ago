799b5461f48b8ce9c5d6ad994726fbe1c599cc44
newtovaux <binaryfrost@gmail.com>
2021-05-18 08:05:00 +0100

    Date checking

--- index.js
+++ index.js
@@ -7,10 +7,21 @@
 "use strict";
 
 exports.ago = function (d) {
+  // Check if d is a Date object
   if (d === void 0) return "undefined";
   if (d === null) return "null";
   if (typeof d !== "object") return "undefined";
   if (d instanceof Date) {
+    let unixts = d.getTime() / 1000;
+    let nowts = new Date().getTime() / 1000;
+    if (nowts < unixts) {
+      return "future";
+    } else {
+      let diffts = nowts - unixts;
+      if (diffts < 60) {
+        return `${diffts} seconds ago`;
+      }
+    }
     return "";
   }
 };
